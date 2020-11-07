731058f49d75c40c3d19b9f3eeb1dfdf77f9e5b3
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-11-07 19:13:30 +0200

    Creating the component for the user information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    UserinfoComponent
   ],
   imports: [
     BrowserModule,
